d8ee5368246f280acec94e0c01419bd93dd61dd1
sayaka <t.sayaka.1438@gmail.com>
2026-06-24 18:07:41 +0900

    feat: ファクトリーとシーダーの作成

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -5,8 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use App\Models\Category;
-use App\Models\Tag;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Contact extends Model
---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $contacts = Contact::factory(20)->create();
+
+        foreach ($contacts as $contact) {
+            $tagIds = Tag::inRandomOrder()
+                ->take(rand(1, 3))
+                ->pluck('id');
+            $contact->tags()->attach($tagIds);
+        }
+    }
+}
